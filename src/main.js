132094e0166c79a2f5bcc2df182ee5e18d908aca
abyanfalah <abyanf410@gmail.com>
2023-08-02 20:20:54 +0700

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// tailwind
+import './assets/tailwindInput.css';
+import './assets/tailwindOutput.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
---
+++ src/views/TestPage.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+	<h1 class="text-xl">Testpage</h1>
+</template>
