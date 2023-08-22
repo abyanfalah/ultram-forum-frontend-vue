6b82f3514a1aa11d1b96de238992b5112d80fb8f
abyanfalah <abyanf410@gmail.com>
2023-08-22 14:44:27 +0700

    thread and post loading spinner OK

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,24 +7,32 @@ import ThreadCard from '../components/ThreadCard.vue';
 import {
 	NCard,
 	NSpace,
+	NSpin,
 	useLoadingBar,
+	useMessage,
 } from 'naive-ui';
 
 const loading = useLoadingBar();
+const msg = useMessage();
 
 const threads = ref();
+const isLoadingThreads = ref(false);
+
 
 async function getAllThreads() {
 	try {
 		loading.start();
+		isLoadingThreads.value = true;
 		const data = (await threadApi.getAll()).data;
 		threads.value = data;
 		loading.finish();
 
 	} catch (error) {
 		loading.error();
+		msg.error('Failed loading threads');
+	} finally {
+		isLoadingThreads.value = false;
 	}
-	// console.log(threads.value);
 }
 
 
@@ -39,6 +47,11 @@ onMounted(() => {
 
 <template>
 	<h1 class="text-3xl">Home</h1>
+
+	<div class="block text-center">
+		<NSpin v-show="isLoadingThreads"></NSpin>
+	</div>
+
 
 	<ThreadCard v-for="thread in threads"
 		:key="thread.id"
--- src/views/ThreadView.vue
+++ src/views/ThreadView.vue
@@ -9,26 +9,45 @@ import PostCard from '../components/PostCard.vue';
 import {
 	NSpace,
 	NAvatar,
+	NSpin,
 	NDivider,
-	NCard
+	NCard,
+	useMessage
 } from 'naive-ui';
 import { useAuthStore } from '../stores/authStore';
 import postApi from '../services/apis/backend/postApi';
 
 const props = defineProps(['slug']);
 const authStore = useAuthStore();
+const msg = useMessage();
 
 const thread = ref({});
 const posts = ref([]);
+const isLoadingPost = ref(false);
+const isLoadingThread = ref(false);
 
 async function getThread() {
-	const data = (await threadApi.get(props.slug)).data;
-	thread.value = data;
+	try {
+		isLoadingThread.value = true;
+		const data = (await threadApi.get(props.slug)).data;
+		thread.value = data;
+	} catch (error) {
+		msg.error('Failed retrieving thread');
+	} finally {
+		isLoadingThread.value = false;
+	}
 }
 
 async function getThreadPosts() {
-	const data = (await postApi.getByThreadSlug(props.slug)).data;
-	posts.value = data;
+	try {
+		isLoadingPost.value = true;
+		const data = (await postApi.getByThreadSlug(props.slug)).data;
+		posts.value = data;
+	} catch (error) {
+		msg.error('Failed retrieving comments');
+	} finally {
+		isLoadingPost.value = false;
+	}
 }
 
 function pushPost(post) {
@@ -44,27 +63,36 @@ onBeforeMount(() => {
 </script>
 
 <template>
-	<!-- user detail and time -->
-	<NSpace justify="space-between"
-		class="mb-4">
-		<div class=" flex space-x-2 items-center text-neutral-400">
-			<NAvatar src="/img/miku.jpg"
-				fallback-src="/img/user-default.svg"
-				size="small"
-				role="button"
-				round
-				@click=""></NAvatar>
-			<span>{{ authStore.user.name }}</span>
-		</div>
-		<span class="text-neutral-400">{{ threadDate(thread?.created_at) }}</span>
-	</NSpace>
-
 	<!-- thread -->
-	<h1 class="text-xl">{{ thread.title }}</h1>
-	<p class="my-4">{{ thread.content }}</p>
-	<LikeDislikeThreadButton v-if="thread?.id"
-		:thread="thread" />
-	<!---->
+	<div class="block text-center"
+		v-show="isLoadingThread">
+		<NSpin></NSpin>
+	</div>
+
+	<div>
+		<!-- user detail and time -->
+		<NSpace justify="space-between"
+			class="mb-4">
+			<div class=" flex space-x-2 items-center text-neutral-400">
+				<NAvatar src="/img/miku.jpg"
+					fallback-src="/img/user-default.svg"
+					size="small"
+					role="button"
+					round
+					@click=""></NAvatar>
+				<span>{{ authStore.user.name }}</span>
+			</div>
+			<span class="text-neutral-400">{{ threadDate(thread?.created_at) }}</span>
+		</NSpace>
+
+		<!-- thread -->
+		<h1 class="text-xl">{{ thread.title }}</h1>
+		<p class="my-4">{{ thread.content }}</p>
+		<LikeDislikeThreadButton v-if="thread?.id"
+			:thread="thread" />
+		<!---->
+	</div>
+	<!--  -->
 
 	<NDivider></NDivider>
 
@@ -72,8 +100,14 @@ onBeforeMount(() => {
 	<NewCommentInput class="mb-4"
 		:threadId="thread.id"
 		@created-new-post="pushPost" />
+	<!--  -->
 
-	<!-- comments -->
+	<!-- comment section -->
+	<div class="block text-center"
+		v-show="isLoadingPost">
+		<NSpin></NSpin>
+	</div>
 	<PostCard v-for="post in posts"
 		:post="post" />
+	<!--  -->
 </template>
